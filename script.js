4e2023898c4ca51222eb4a2ee2af8f203563df68
pat20ow <89689604+pat2ow@users.noreply.github.com>
2024-05-16 22:38:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+//use to get the length to use for stroke offset and array
+//change selector in line 4 to get the length of a specific path
+
+// var path = document.querySelector('#stem6');
+// var length = path.getTotalLength();
+// console.log(length);
